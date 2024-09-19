4ed0a4ac8dfa8f2fea006782732a212d5909159f
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-09-18 21:06:11 -0300

    Agrega hilt configurations

---
+++ app/src/main/java/edu/ucne/eliezerterrero_p1_ap2/EliezerTerrero_P1_Ap2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.eliezerterrero_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EliezerTerrero_P1_Ap2App(): Application(){
+
+}
--- app/src/main/java/edu/ucne/eliezerterrero_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/eliezerterrero_p1_ap2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.eliezerterrero_p1_ap2.ui.theme.EliezerTerrero_P1_AP2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
